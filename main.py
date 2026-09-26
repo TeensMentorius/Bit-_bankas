340f860c2a5e8fdf69dfd682abf4c555864e84d0
matasmickunai-source <matas.mickunai@gmail.com>
2026-09-26 12:13:18 +0300

    Update main.py
    realizuota funkcija gauti klientu balansa

--- main.py
+++ main.py
@@ -156,7 +156,13 @@ def rasti_klienta(ieskomo_kliento_id):
     pass
 
 def gauti_balansa(kliento_id):
-    pass
+    
+    for i in range(len(klientu_id)):
+
+        if klientu_id[i] == kliento_id:
+            return balansai[i]
+
+    return None
 
 def gauti_turtingiausia_klienta():
     pass
