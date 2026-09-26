d3e9b164a269019a1010c2ccaccc9e56c50978f0
jj-vv <jonas.jaudegis@gmail.com>
2026-09-26 11:39:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -174,7 +174,10 @@ def gauti_vyriausia_klienta():
     pass
 
 def gauti_jauniausia_klienta():
-    pass
+    jauniausias = min(amzius)
+    jauniausias_vardas = vardai[amzius.index(jauniausias)]
+    jauniausias_pavarde = pavardes[amzius.index(jauniausias)]
+    return jauniausias, jauniausias_vardas, jauniausias_pavarde
 
 def atrinkti_turtingus_klientus():
     pass
@@ -201,3 +204,4 @@ def sukurti_nauja_banko_saskaita(
 
 #Funkcijas paleidžiame šioje vietoje
 
+print("Jauniausias klientas : ", gauti_jauniausia_klienta())
